17bab6f73fa38477970466bf64aca9f4a200d39f
Alex Nabrozidis <nabrozidhs@gmail.com>
2017-01-09 23:30:33 +0000

    Finish day2.

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -7,39 +7,49 @@ import java.lang.Integer.min
 enum class Direction {UP, RIGHT, DOWN, LEFT }
 data class Position(val x: Int, val y: Int)
 
-val keypad = listOf(
-        listOf(1, 2, 3),
-        listOf(4, 5, 6),
-        listOf(7, 8, 9)
+val keypad1 = listOf(
+        listOf('1', '2', '3'),
+        listOf('4', '5', '6'),
+        listOf('7', '8', '9')
+)
+val keypad2 = listOf(
+        listOf(' ', ' ', '1', ' ', ' '),
+        listOf(' ', '2', '3', '4', ' '),
+        listOf('5', '6', '7', '8', '9'),
+        listOf(' ', 'A', 'B', 'C', ' '),
+        listOf(' ', ' ', 'D', ' ', ' ')
 )
 
-fun trackAllDirections(directions: List<List<Direction>>, keypad: List<List<Int>>): List<List<Position>> {
-    val positions = mutableListOf<List<Position>>()
+fun trackAllDirections(directions: List<List<Direction>>, keypad: List<List<Char>>): List<Position> {
+    val positions = mutableListOf<Position>()
     for (direction in directions) {
-        val linePositions: List<Position>
+        val linePosition: Position
         if (positions.size == 0) {
-            linePositions = trackDirections(direction, Position(1, 1), keypad)
+            linePosition = trackDirection(direction, Position(1, 1), keypad)
         } else {
-            linePositions = trackDirections(direction, positions.last().last(), keypad)
+            linePosition = trackDirection(direction, positions.last(), keypad)
         }
-        positions.add(linePositions)
+        positions.add(linePosition)
     }
     return positions
 }
 
-fun trackDirections(directions: List<Direction>, startingPosition: Position, keypad: List<List<Int>>): List<Position> {
+fun trackDirection(directions: List<Direction>, startingPosition: Position, keypad: List<List<Char>>): Position {
     var position = startingPosition
-    val positions = mutableListOf<Position>()
     for (direction in directions) {
+        val newPosition: Position
         when (direction) {
-            Direction.UP -> position = Position(position.x, max(position.y - 1, 0))
-            Direction.DOWN -> position = Position(position.x, min(position.y + 1, keypad.size - 1))
-            Direction.LEFT -> position = Position(max(position.x - 1, 0), position.y)
-            Direction.RIGHT -> position = Position(min(position.x + 1, keypad[0].size - 1), position.y)
+            Direction.UP -> newPosition = Position(position.x, max(position.y - 1, 0))
+            Direction.DOWN -> newPosition = Position(position.x, min(position.y + 1, keypad.size - 1))
+            Direction.LEFT -> newPosition = Position(max(position.x - 1, 0), position.y)
+            Direction.RIGHT -> newPosition = Position(min(position.x + 1, keypad[0].size - 1), position.y)
         }
-        positions.add(position)
+
+        if (keypad[newPosition.y][newPosition.x] != ' ') {
+            position = newPosition
+        }
     }
-    return positions
+    return position
 }
 
 fun parse(input: String): List<List<Direction>> =
@@ -56,12 +66,15 @@ fun parse(input: String): List<List<Direction>> =
                     }
                 }
 
-fun convertToPin(positions: List<List<Position>>, keypad: List<List<Int>>): String =
-        positions.map { keypad[it.last().y][it.last().x] }.fold("", { s, e -> s + e })
+fun convertToPin(positions: List<Position>, keypad: List<List<Char>>): String =
+        positions.map { keypad[it.y][it.x] }.fold("", { s, e -> s + e })
 
 fun main(args: Array<String>) {
-    val directions = trackAllDirections(parse(File("data/day02/input.txt").readText()), keypad)
+    val directions1 = trackAllDirections(parse(File("data/day02/input.txt").readText()), keypad1)
 
-    println(directions)
-    println(convertToPin(directions, keypad))
+    println(convertToPin(directions1, keypad1))
+
+    val directions2 = trackAllDirections(parse(File("data/day02/input.txt").readText()), keypad2)
+
+    println(convertToPin(directions2, keypad2))
 }
