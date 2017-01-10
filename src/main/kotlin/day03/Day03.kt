95289858ab9c06b9d8deb7ec6755b9092a506fa9
Alex Nabrozidis <nabrozidhs@gmail.com>
2017-01-10 01:16:57 +0000

    Finish day3.

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -15,8 +15,23 @@ fun parse(input: String): List<Triple<Int, Int, Int>> =
                     Triple(a.toInt(), b.toInt(), c.toInt())
                 }
 
+fun parse2(input: String): List<Triple<Int, Int, Int>> {
+    val lines = input.split("\n")
+            .map {
+                val (unused, a, b, c) = parseRegex.find(it)!!.groupValues
+                listOf(a.toInt(), b.toInt(), c.toInt())
+            }
+    val triples = mutableListOf<Triple<Int, Int, Int>>()
+    for (i in 0..lines.size-1 step 3) {
+        (0..2).mapTo(triples) { Triple(lines[i][it], lines[i+1][it], lines[i+2][it]) }
+    }
+    return triples
+}
+
 fun main(args: Array<String>) {
     val triangles = parse(File("data/day03/input.txt").readText())
+    val triangles2 = parse2(File("data/day03/input.txt").readText())
 
     println("Valid triangles: ${triangles.filter { it.isTriangle() }.count()}")
+    println("Valid triangles2: ${triangles2.filter { it.isTriangle() }.count()}")
 }
