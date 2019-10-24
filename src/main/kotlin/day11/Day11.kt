ddc5cd58ba6a3b335242651e3ff97efd7a58677d
Alex Nabrozidis <nabrozidhs@gmail.com>
2019-10-24 17:55:41 +0100

    day 11

---
+++ src/main/kotlin/day11/Day11.kt
@@ -0,0 +1,108 @@
+package day11
+
+// I've done it manually :D
+/*
+
+0
+F4 .  .  .  .  .  .  .  .  .  .  .
+F3 .  PG PM RG RM .  .  .  .  .  .
+F2 .  .  .  .  .  .  PM .  .  SM .
+F1 E  .  .  .  .  PG .  TG TM .  SG
+
+...
+
+5
+F4 .  .  .  .  .  .  .  .  .  .  .
+F3 .  PG PM RG RM .  .  .  .  .  .
+F2 E  .  .  .  .  PG PM TG TM SM SG
+F1 .  .  .  .  .  .  .  .  .  .  .
+
+...
+
+10
+F4 .  .  .  .  .  .  .  .  .  .  .
+F3 E  PG PM RG RM PG .  TG .  SG SM
+F2 .  .  .  .  .  .  PM .  TM .  .
+F1 .  .  .  .  .  .  .  .  .  .  .
+
+...
+
+14
+F4 .  .  .  .  .  .  .  .  .  .  .
+F3 E  PG PM RG RM PG PM TG TM SG SM
+F2 .  .  .  .  .  .  .  .  .  .  .
+F1 .  .  .  .  .  .  .  .  .  .  .
+
+...
+
+24
+F4 .  PG .  RG .  PG .  TG .  SG .
+F3 E  .  PM .  RM .  PM .  TM .  SM
+F2 .  .  .  .  .  .  .  .  .  .  .
+F1 .  .  .  .  .  .  .  .  .  .  .
+
+...
+
+31
+F4 E  PG PM RG RM PG PM TG TM SG SM
+F3 E  .  .  .  .  .  .  .  .  .  .
+F2 .  .  .  .  .  .  .  .  .  .  .
+F1 .  .  .  .  .  .  .  .  .  .  .
+
+// PART 2
+
+0
+F4 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+F3 .  PG PM RG RM .  .  .  .  .  .  .  .  .  .
+F2 .  .  .  .  .  .  PM .  .  .  SM .  .  .  .
+F1 E  .  .  .  .  PG .  TG TM SG .  EG EM DG DM
+
+...
+
+8
+F4 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+F3 .  PG PM RG RM .  .  .  .  .  .  .  .  .  .
+F2 .  .  .  .  .  PG PM TG .  SG .  EG .  DG .
+F1 E  .  .  .  .  .  .  .  TM .  SM .  EM .  DM
+
+...
+
+13
+F4 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+F3 .  PG PM RG RM .  .  .  .  .  .  .  .  .  .
+F2 E  .  .  .  .  PG PM TG TM SG SM EG EM DG DM
+F1 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+
+...
+
+23
+F4 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+F3 .  PG PM RG RM PG .  TG .  SG .  EG .  DG .
+F2 E  .  .  .  .  .  PM .  TM .  SM .  EM .  DM
+F1 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+
+...
+
+30
+F4 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+F3 E  PG PM RG RM PG PM TG TM SG SM EG EM DG DM
+F2 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+F1 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+
+..
+
+44
+F4 .  PG .  RG .  PG .  TG .  SG .  EG .  DG .
+F3 E  .  PM .  RM .  PM .  TM .  SM .  EM .  DM
+F2 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+F1 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+
+...
+
+55
+F4 E  PG PM RG RM PG PM TG TM SG SM EG EM DG DM
+F3 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+F2 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+F1 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+
+ */
